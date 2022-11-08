bde1bdb839d26d8115839945da0943ba7d74802d
Aludeku <luisfelipemartins1198@gmail.com>
2022-11-08 18:33:56 -0300

    new lines

--- code/level.py
+++ code/level.py
@@ -7,6 +7,7 @@ from support import *
 from random import choice
 from weapon import Weapon
 from ui import UI
+from enemy import Enemy
 
 class Level:
 	def __init__(self):
@@ -32,6 +33,7 @@ class Level:
 			'boundary' : import_csv_layout('../map/map_FloorBlocks.csv'),
 			'grass': import_csv_layout('../map/map_Grass.csv'),
 			'object': import_csv_layout('../map/map_Objects.csv'),
+			'entities': import_csv_layout('../map/map_Entities.csv')
 		}
 		graphics = {
 			'grass': import_folder('../graphics/Grass'),
@@ -53,17 +55,18 @@ class Level:
 						if style == 'object':
 							surf = graphics['objects'][int(col)]
 							Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'object',surf)
-		#		if col == 'x':
-		#			Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
-		#		if col == 'p':
-		#			self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites)
-		self.player = Player(
-			(2000, 1330),
-			[self.visible_sprites],
-			self.obstacle_sprites,
-			self.create_attack,
-			self.destroy_attack,
-			self.create_magic)
+
+						if style == 'entities':
+							if col == '394':
+								self.player = Player(
+									(x,y),
+									[self.visible_sprites],
+									self.obstacle_sprites,
+									self.create_attack,
+									self.destroy_attack,
+									self.create_magic)
+							else:
+								Enemy('monster',(x,y), [self.visible_sprites])
 
 	def create_attack(self):
 		self.current_attack = Weapon(self.player, [self.visible_sprites])
